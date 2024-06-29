0f3169428f6a527190c55dea1d2756afa13867ba
VictorMarimon <3reehuy@gmail.com>
2024-06-28 20:44:18 -0500

    feat archivo datos (funciones json)

---
+++ MODULO_DATOS/datos.py
@@ -0,0 +1,17 @@
+import json
+
+def cargar_datos(archivo):
+    datos = {}
+    with open(archivo,"r") as file:
+        datos=json.load(file)
+    return datos
+        
+        
+
+def guardar_datos(datos, archivo):
+    datos = dict(datos)
+    
+    diccionario=json.dumps(datos, indent=4)
+    file=open(archivo,"w")
+    file.write(diccionario)
+    file.close()

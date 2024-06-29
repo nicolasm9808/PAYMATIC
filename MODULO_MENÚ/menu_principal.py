0430f72fbabe4d35b88fa0fe176caf9db9aefbf1
Victor Andres Marimon Mendoza <vmarimon1@udi.edu.co>
2024-06-29 18:34:24 -0500

    feat menu usuarios

--- MODULO_MENÚ/menu_principal.py
+++ MODULO_MENÚ/menu_principal.py
@@ -2,13 +2,12 @@ import sys
 
 sys.path.append("..")
 
-from MODULO_MENÚ.menu_usuario import menu_usuario
-from MODULO_MENÚ.menu_entrada_salida import *
-from MODULO_MENÚ.menu_nómina import *
-from MODULO_DATOS.datos import *
+from MODULO_MENÚ.menu_entrada_salida import menu_entrada_salida
+from MODULO_MENÚ.menu_nómina import menu_nómina
+from MODULO_FUNCIONES.usuario import *
 
-def menu_principal():
-    print("Bienvenid@ a ServiPro")
+def menu():
+    print("Bienvenid@ a PayMatic")
     print("")
     while True:
         print("")
@@ -22,14 +21,47 @@ def menu_principal():
         print("")
 
         if opcion == "1":
-            menu_usuario();
+            menu_usuario()
         elif opcion == "2":
-            menu_nómina();
+            menu_nómina()
         elif opcion == "3":
-            menu_entrada_salida();
+            menu_entrada_salida()
         elif opcion == "4":
             print("")
             print("Programa Finalizado")
+            break
+        else:
+            print("")
+            print("Opción Incorrecta")
+            print("")
+
+def menu_usuario():
+    print("Módulo Usuarios")
+    print("")
+    while True:
+        print("")
+        print("1- Agregar usuario")
+        print("2- Modificar usuario")
+        print("3- Eliminar usuario")
+        print("4- Listar usuarios")
+        print("5- Cerrar Programa")
+        print("")
+
+        opcion = input("Selecciona una opcion: ")
+        print("")
+
+        if opcion == "1":
+            agregar()
+        elif opcion == "2":
+            modificar()
+        elif opcion == "3":
+            eliminar()
+        elif opcion == "4":
+            leer()
+        elif opcion == "5":
+            print("")
+            print("Regreseando...")
+            menu()
             break
         else:
             print("")
